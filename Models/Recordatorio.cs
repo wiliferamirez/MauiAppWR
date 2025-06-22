a888494244eb280db282ffcd4a5dc6b29df2fb4a
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-22 18:54:57 -0500

    Agregar nueva clase para recordatorio

---
+++ Models/Recordatorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppWR.Models
+{
+    internal class Recordatorio
+    {
+        public string Texto { get; set; }
+        public TimeSpan FechaHora { get; set; }
+        public bool Activo { get; set; }
+    }
+}
