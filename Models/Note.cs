5a4c70a1ab237ca18227d563c7b656a0bddc5cc5
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-06-22 18:19:13 -0500

    Agregar metodo apara guardar, borrar y cargar las notas

--- Models/Note.cs
+++ Models/Note.cs
@@ -11,5 +11,28 @@ namespace MauiAppWR.Models
         public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        public void Save() =>
+            File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void Delete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static Note Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+
     }
 }
